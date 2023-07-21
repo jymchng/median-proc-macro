f8546d3f2cf888b9abfa09e27b4aa00e657376b9
Soon Siang <jymchng@hotmail.com>
2023-07-22 04:26:01 +0800

    checked the types too

--- crates/median-pm-core/src/lib.rs
+++ crates/median-pm-core/src/lib.rs
@@ -10,7 +10,7 @@ mod implement;
 pub fn median(ts: TokenStream) -> TokenStream {
     if ts.is_empty() {
         return quote!(
-            0
+            0.0
         ).into()
     }
     implement::median_impl(ts).into()
